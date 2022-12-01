75ccd3d63a203ccbec080f46c473996dcad42706
Fabricio Magnus <fabricio.magnus92@outlook.com>
2022-11-30 21:53:17 -0300

    fix: image error

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,32 +26,41 @@ class MyApp extends StatelessWidget {
             leading: Container(),
             title: Text('Tasks'),
           ),
-          body: ListView(
-            children: [
-              Task(
-                  'Aprender Flutter',
-                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                  3),
-              Task(
-                  'Andar de Bike',
-                  'https://aventurasnabike.com.br/wp-content/uploads/2022/05/bicicleta-emagrece.jpg',
-                  2),
-              Task(
-                  'Meditar',
-                  'https://miro.medium.com/max/1400/1*tf_mOK5k59ENA8yZ_I72dQ.jpeg',
-                  5),
-              Task(
-                  'Ler',
-                  'https://thumbs.jusbr.com/imgs.jusbr.com/publications/artigos/images/1427507524.jpg',
-                  4),
-              Task(
-                  'Jogar',
-                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                  1),
-            ],
+          body: AnimatedOpacity(
+            opacity: opacidade ? 1 : 0,
+            duration: Duration(milliseconds: 900),
+            child: ListView(
+              children: [
+                Task(
+                    'Aprender Flutter',
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                    3),
+                Task(
+                    'Andar de Bike',
+                    'https://aventurasnabike.com.br/wp-content/uploads/2022/05/bicicleta-emagrece.jpg',
+                    2),
+                Task(
+                    'Meditar',
+                    'https://miro.medium.com/max/1400/1*tf_mOK5k59ENA8yZ_I72dQ.jpeg',
+                    5),
+                Task(
+                    'Ler',
+                    'https://thumbs.jusbr.com/imgs.jusbr.com/publications/artigos/images/1427507524.jpg',
+                    4),
+                Task(
+                    'Jogar',
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                    1),
+              ],
+            ),
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              setState(() {
+                opacidade = !opacidade;
+              });
+            },
+            child: Icon(Icons.remove_red_eye),
           ),
         ));
   }
