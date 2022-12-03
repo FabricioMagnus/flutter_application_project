fe92b0d78780d8651edc9fe0685c87c9770919ee
Fabricio Magnus <fabricio.magnus92@outlook.com>
2022-12-02 21:39:05 -0300

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/form-screen.dart';
 import 'package:flutter_application_1/screens/initial_screen.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InitiaScreen(),
+      home: const FormScreen(),
     );
   }
 }
